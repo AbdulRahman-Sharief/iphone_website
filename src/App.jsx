8957e11e438be1af99c2de37b3c9b109021b9c6d
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-03-18 00:48:39 +0200

    Chip

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import * as Sentry from '@sentry/react';
 import Features from "./components/Features";
+import Chip from './components/Chip';
 const App = () => {
   return (
     <main className="bg-black">
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <Chip />
     </main>
   );
 };
