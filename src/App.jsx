c608f40643692b6cbf401c5c3818b9c21cecd8d4
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-03-18 00:22:35 +0200

    Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import * as Sentry from '@sentry/react';
+import Features from "./components/Features";
 const App = () => {
   return (
     <main className="bg-black">
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   );
 };
