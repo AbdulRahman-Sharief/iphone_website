60e8aa1af71787621fda4e5b25076a7c65b24c6f
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-03-18 00:59:02 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Model from "./components/Model";
 import * as Sentry from '@sentry/react';
 import Features from "./components/Features";
 import Chip from './components/Chip';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <main className="bg-black">
@@ -14,6 +15,7 @@ const App = () => {
       <Model />
       <Features />
       <Chip />
+      <Footer />
     </main>
   );
 };
